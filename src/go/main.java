79a25f3117260b0136aee655c74ab4bb8968cbe3
Mathys Visticot <mathys.vst@gmail.com>
2022-09-27 15:35:29 +0200

    refactor(project): reroute code in src file

---
+++ src/go/main.java
@@ -0,0 +1,12 @@
+package go;
+
+import bo.*;
+import bo.Process;
+
+public class main {
+
+    public static void main(String[] args) {
+        Process myProcess = new Process();
+        myProcess.processStart();
+    }
+}
